e4f436ea787dd2463acea400db3d1b7678972c26
Dmitry Konishchev <konishchev@gmail.com>
2013-07-02 18:54:10 +0400

    Fix validation

--- python_config.py
+++ python_config.py
@@ -5,16 +5,28 @@ from __future__ import unicode_literals
 import imp
 import sys
 
-PY2 = sys.version_info < (3,)
-if PY2:
+_PY2 = sys.version_info < (3,)
+if _PY2:
     str = unicode
+
+_BASIC_TYPES = (int, float, bytes, str)
+"""Python basic types."""
+
+if _PY2:
+    _BASIC_TYPES += (long,)
+
+_COMPLEX_TYPES = (tuple, list, set, dict)
+"""Python complex types."""
+
+_VALID_TYPES = _BASIC_TYPES + _COMPLEX_TYPES
+"""Option value must be one of these types."""
 
 
 class Error(Exception):
     """The base class for all exceptions that the module raises."""
 
-    def __init__(self, error, *args):
-        super(Error, self).__init__(error.format(*args) if args else error)
+    def __init__(self, error, *args, **kwargs):
+        super(Error, self).__init__(error.format(*args, **kwargs) if args or kwargs else error)
 
 
 class FileReadingError(Error):
@@ -37,19 +49,18 @@ class ParsingError(Error):
 class ValidationError(Error):
     """Error during validation of a configuration file."""
 
-    def __init__(self, path, option_name, error):
+    def __init__(self, path, error):
         super(ValidationError, self).__init__(
             "Error while parsing '{0}' configuration file: {1}.", path, error)
-        self.option_name = option_name
+        self.option_name = error.option_name
 
 
 class _ValidationError(Error):
     """Same as ValidationError, but for internal usage."""
 
-    def __init__(self, option_name, *args):
-        super(_ValidationError, self).__init__(*args)
+    def __init__(self, option_name, *args, **kwargs):
+        super(_ValidationError, self).__init__(*args, **kwargs)
         self.option_name = option_name
-
 
 
 
@@ -73,65 +84,86 @@ def load(path, contents=None):
 
     config = {}
 
-    for key, value in config_module.__dict__.items():
-        if not key.startswith("_") and key.isupper():
+    for option, value in config_module.__dict__.items():
+        if not option.startswith("_") and option.isupper():
             try:
-                config[key] = _validate_value(key, value)
+                config[option] = _validate_value(option, value)
             except _ValidationError as e:
-                raise ValidationError(path, e.option_name, e)
+                raise ValidationError(path, e)
 
     return config
 
 
-def _validate_value(key, value):
-    """Validates a configuration file value."""
-
-    valid_types = ( dict, set, list, tuple, str, bytes, int, float )
-    if PY2:
-        valid_types += ( long, )
+def _validate_value(option, value, valid_types=_VALID_TYPES):
+    """Validates an option value."""
 
     value_type = type(value)
 
     if value_type not in valid_types:
-        raise _ValidationError(key, "{0} has an invalid value type ({1}). Allowed types: {2}.",
-            key, value_type.__name__, ", ".join(t.__name__ for t in valid_types))
+        raise _ValidationError(option,
+            "{option} has an invalid value type ({type}). Allowed types: {valid_types}.",
+            option=option, type=value_type.__name__,
+            valid_types=", ".join(t.__name__ for t in valid_types))
 
-    if value_type is bytes:
+    if value_type is dict:
+        value = _validate_dict(option, value)
+    elif value_type is list:
+        value = _validate_list(option, value)
+    elif value_type is tuple:
+        value = _validate_tuple(option, value)
+    elif value_type is set:
+        value = _validate_set(option, value)
+    elif value_type is bytes:
         try:
             value = value.decode()
         except UnicodeDecodeError as e:
-            raise _ValidationError(key, "{0} has an invalid value: {1}.", key, e)
-    elif value_type is dict:
-        value = _validate_dict_value(key, value)
-    elif value_type in (list, tuple, set):
-        value = _validate_list_like_value(key, value)
+            raise _ValidationError(option, "{0} has an invalid value: {1}.", option, e)
 
     return value
 
 
-def _validate_dict_value(key, value):
-    """Validates a dict value."""
+def _validate_dict(option, dictionary):
+    """Validates a dictionary."""
 
-    new_value = {}
+    for key, value in tuple(dictionary.items()):
+        valid_key = _validate_value("A {0}'s key".format(option),
+            key, valid_types=_BASIC_TYPES)
 
-    for subkey, subvalue in value.items():
-        subkey = _validate_value("A {0}'s key".format(key), subkey)
-        subvalue = _validate_value("{0}[{1}]".format(key, repr(subkey)), subvalue)
-        new_value[subkey] = subvalue
+        valid_value = _validate_value("{0}[{1}]".format(option, repr(key)), value)
 
-    return new_value
+        if valid_key is not key:
+            del dictionary[key]
+            dictionary[valid_key] = valid_value
+        elif valid_value is not value:
+            dictionary[valid_key] = valid_value
+
+    return dictionary
 
 
-def _validate_list_like_value(key, value):
-    """Validates a list-like value."""
+def _validate_list(option, sequence):
+    """Validates a list."""
 
-    if type(value) is set:
-        return [
-            _validate_value("A {0}'s key".format(key), subvalue)
-            for subvalue in value
-        ]
-    else:
-        return [
-            _validate_value("{0}[{1}]".format(key, repr(index)), subvalue)
-            for index, subvalue in enumerate(value)
-        ]
+    for index, value in enumerate(sequence):
+        valid_value = _validate_value("{0}[{1}]".format(option, index), value)
+        if valid_value is not value:
+            sequence[index] = valid_value
+
+    return sequence
+
+
+def _validate_tuple(option, sequence):
+    """Validates a tuple."""
+
+    return [
+        _validate_value("{0}[{1}]".format(option, index), value)
+        for index, value in enumerate(sequence)
+    ]
+
+
+def _validate_set(option, sequence):
+    """Validates a set."""
+
+    return [
+        _validate_value("A {0}'s key".format(option), value)
+        for value in sequence
+    ]
